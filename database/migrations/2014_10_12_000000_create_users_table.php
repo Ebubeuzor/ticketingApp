c2d36c5246a631096725f09af84a78324cbf0e93
Ebube Uzor <ebubeuzor17@gmail.com>
2024-03-29 20:55:12 +0100

    Add 'remember\_token' column to users table
    This adds a new string column called 'remember\_token' to the 'users' table.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('remember_token');
             $table->string('password');
             $table->timestamps();
         });
